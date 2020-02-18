4626c131425126dcd803193c1aef12a531a7ae28
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2020-02-18 16:31:58 -0500

    Added a computed column to the tbl_ref_sla_translation table that should replace the need for the tbl_ref_sla_code table. However, will need to rethink how the data is inserted. Closes #44.

--- tbl_ref_sla_translation.sql
+++ tbl_ref_sla_translation.sql
@@ -3,7 +3,7 @@
  Created By: Dan Gallagher, daniel.gallagher@parks.nyc.gov, Innovation & Performance Management         											   
  Modified By: Dan Gallagher, daniel.gallagher@parks.nyc.gov, Innovation & Performance Management																						   			          
  Created Date:  08/19/2019																							   
- Modified Date: 12/27/2019																							   
+ Modified Date: 02/18/2020																							   
 											       																	   
  Project: SLADB	
  																							   
@@ -15,7 +15,7 @@
 create table sladb.dbo.tbl_ref_sla_translation(sla_trans_id int identity(1,1) primary key,				   
 											   sla_id nvarchar(1) not null foreign key references sladb.dbo.tbl_ref_sla(sla_id),
 											   date_category_id int not null foreign key references sladb.dbo.tbl_ref_sla_season_category(date_category_id),
-											   sla_code int not null foreign key references sladb.dbo.tbl_ref_sla_code(sla_code),
+											   sla_code as ceiling(sla_trans_id/2) persisted,
 											   /*Create a unique constraint to make sure that each sla and season only appear once per combination.*/
 											   constraint unq_sla_code unique (sla_id, date_category_id, sla_code));
 
@@ -33,21 +33,21 @@ insert into @sla_tab(sla_id, date_category_id)
 
 with combos as(
 select l.*, 
-	   r.sla_id as sla_id2,
-	   row_number() over(partition by l.date_category_id order by l.date_category_id, l.sla_id) as sla_code
+	   r.sla_id as sla_id2/*,
+	   row_number() over(partition by l.date_category_id order by l.date_category_id, l.sla_id) as sla_code*/
 from @sla_tab as l
 cross join
 	 @sla_tab as r
 where l.date_category_id != r.date_category_id)
 
-insert into sladb.dbo.tbl_ref_sla_translation(sla_id,
-											  date_category_id,
-											  sla_code)
+--insert into sladb.dbo.tbl_ref_sla_translation(sla_id,
+--											  date_category_id,
+--											  sla_code)
 	select case when date_category_id = 1 then sla_id
 			else sla_id2
 		end as sla_id,
-	   date_category_id,
-	   sla_code
+	   date_category_id/*,
+	   sla_code*/
 from combos
 
 --truncate table sladb.dbo.tbl_ref_sla_translation
