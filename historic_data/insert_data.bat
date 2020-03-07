22219d2abcf42ae686230bf9c25c8b46cdf52312
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2020-03-06 21:26:38 -0500

    Updated batchfiles to allow seamless deployment and data insertion without hardcoding servers or filepaths. Closes #12

--- historic_data/insert_data.bat
+++ historic_data/insert_data.bat
@@ -1,32 +1,34 @@
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\historic_data\sp_i_tbl_unit_sla_season_historic.sql
+sqlcmd -S . -E -i sp_i_tbl_unit_sla_season_historic.sql
 
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\historic_data\insert_tbl_ref_sla_season_day_rank.sql
+sqlcmd -S . -E -i insert_tbl_ref_sla_season_day_rank.sql
 
-sqlcmd -S dpr-vdipm001 -E -Q "exec sladb.dbo.sp_insert_tbl_ref_calendar;"
+sqlcmd -S . -E -Q "exec sladb.dbo.sp_insert_tbl_ref_calendar;"
 
-sqlcmd -S dpr-vdipm001 -E -Q "exec sladb.dbo.sp_merge_ref_unit;"
+sqlcmd -S . -E -Q "exec sladb.dbo.sp_merge_ref_unit;"
 
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\historic_data\insert_tbl_ref_sla.sql
+sqlcmd -S . -E -i insert_tbl_ref_sla.sql
 
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\historic_data\insert_tbl_ref_sla_season_category.sql
+sqlcmd -S . -E -i insert_tbl_ref_sla_season_category.sql
 
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\historic_data\insert_tbl_ref_sla_season_date_type.sql
+sqlcmd -S . -E -i insert_tbl_ref_sla_season_date_type.sql
 
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\historic_data\insert_tbl_ref_sla_translation.sql
+sqlcmd -S . -E -i insert_tbl_ref_sla_translation.sql
 
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\historic_data\insert_tbl_ref_sla_season_day_name.sql
+sqlcmd -S . -E -i insert_tbl_ref_sla_season_day_name.sql
 
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\historic_data\insert_tbl_ref_sla_change_status.sql
+sqlcmd -S . -E -i insert_tbl_ref_sla_change_status.sql
 
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\historic_data\insert_tbl_ref_sla_season_month_name.sql
+sqlcmd -S . -E -i insert_tbl_ref_sla_season_month_name.sql
 
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\historic_data\insert_historic_seasons.sql
+sqlcmd -S . -E -i insert_historic_seasons.sql
 
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\historic_data\insert_historic_change_requests.sql
+sqlcmd -S . -E -i insert_historic_change_requests.sql
 
-sqlcmd -S dpr-vdipm001 -E -Q "exec sladb.dbo.sp_season_dates;"
+sqlcmd -S . -E -Q "exec sladb.dbo.sp_season_dates;"
 
-sqlcmd -S dpr-vdipm001 -E -i C:\Projects\sladb\sp_i_tbl_unit_sla_season.sql
+cd ..
+
+sqlcmd -S . -E -i sp_i_tbl_unit_sla_season.sql
 
 pause
 
