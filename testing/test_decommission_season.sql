2fe2eede379667723ad5a363c1388e1bacd63df5
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2020-08-14 20:54:30 -0400

    Test decommissioning a season by setting the value of effective = 0, related to #124.

---
+++ testing/test_decommission_season.sql
@@ -0,0 +1,25 @@
+/***********************************************************************************************************************
+																													   	
+ Created By: Dan Gallagher, daniel.gallagher@parks.nyc.gov, Innovation & Performance Management         											   
+ Modified By: <Modifier Name>																						   			          
+ Created Date:  <MM/DD/YYYY>																							   
+ Modified Date: <MM/DD/YYYY>																							   
+											       																	   
+ Project: <Project Name>	
+ 																							   
+ Tables Used: <Database>.<Schema>.<Table Name1>																							   
+ 			  <Database>.<Schema>.<Table Name2>																								   
+ 			  <Database>.<Schema>.<Table Name3>				
+			  																				   
+ Description: <Lorem ipsum dolor sit amet, legimus molestiae philosophia ex cum, omnium voluptua evertitur nec ea.     
+	       Ut has tota ullamcorper, vis at aeque omnium. Est sint purto at, verear inimicus at has. Ad sed dicat       
+	       iudicabit. Has ut eros tation theophrastus, et eam natum vocent detracto, purto impedit appellantur te	   
+	       vis. His ad sonet probatus torquatos, ut vim tempor vidisse deleniti.>  									   
+																													   												
+***********************************************************************************************************************/
+/*Set the value of effective = 0 for a season that currently has an effective = 1 value*/
+begin transaction
+	update sladb.dbo.tbl_sla_season
+		set effective = 0 
+		where season_id = 11;
+commit;
