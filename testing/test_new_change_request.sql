d65b6261275ae819073145fdcc3d9130dd357a89
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2020-08-14 22:09:45 -0400

    Added testing scenario for #131.

--- testing/test_new_change_request.sql
+++ testing/test_new_change_request.sql
@@ -34,3 +34,21 @@ insert into @new_change_request(unit_id,
 	from sladb.dbo.vw_unit_sla_season_unassigned;
 
 exec sladb.dbo.sp_insert_change_request @new_change_request = @new_change_request, @auto_approve = 1;
+
+/*Insert 4 records for where the effective_start date is after the effective_end date of the season they are assigned*/
+declare @new_change_request as insert_change_request;
+
+insert into @new_change_request(unit_id,
+								sla_code,
+								season_id,
+								/*Make sure that the effective start date is greater than or equal to today's date.*/
+								effective_start,
+								change_request_justification)
+	select top 4 unit_id,
+		   2 as sla_code,
+		   12 as season_id,
+		   dateadd(day, 2, cast(getdate() as date)) as effective_start,
+		   'Testing inserting new records.' 
+	from sladb.dbo.vw_unit_sla_season_unassigned;
+
+exec sladb.dbo.sp_insert_change_request @new_change_request = @new_change_request, @auto_approve = 1;
