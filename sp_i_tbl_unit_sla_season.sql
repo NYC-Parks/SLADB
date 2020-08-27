005d22316dd87a016325890b53da2a4b4b57269e
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2020-08-27 16:36:48 -0400

    Updated logic of sp_i_tbl_unit_sla_season. I believe that this closes #152. This for this to function, the existing information in the database needs to be updated. The diagrams also need to be updated.

--- sp_i_tbl_unit_sla_season.sql
+++ sp_i_tbl_unit_sla_season.sql
@@ -30,6 +30,9 @@ create or alter procedure dbo.sp_i_tbl_unit_sla_season as
 		/*Execute the stored procedure to check for and flag any invalid change requests*/
 		exec sladb.dbo.sp_u_tbl_change_request;
 
+		/*Calculate the difference in minutes between the current utc time and the current local time.*/
+		declare @number int = (select datediff(minute, getutcdate(), getdate()))
+
 		begin transaction
 			insert into sladb.dbo.tbl_unit_sla_season(unit_id, sla_code, season_id, effective, effective_start, change_request_id, created_date_utc)
 				select unit_id, 
@@ -39,18 +42,22 @@ create or alter procedure dbo.sp_i_tbl_unit_sla_season as
 					   effective_start,
 					   change_request_id,
 					   getutcdate() as created_date_utc 
-				from(
-			    /*Join the change request table to the change request status table based on the change_request_id*/
-				select unit_id, 
-					   sla_code, 
-					   season_id,  
-					   effective_start,
-					   change_request_id
-				from sladb.dbo.tbl_change_request
-				/*If the change request status (sla_change_status) is 2 = Approved and the effective_start_adj (adjusted effective_start_date)
-					is equal to the current date minus one hour then insert the new record*/
-				where sla_change_status = 2 and
-					  effective_start_adj <= cast(dateadd(hour, -1, getdate()) as date)
+				from(select l.unit_id, 
+							l.sla_code, 
+							l.season_id,
+							/*If the created_date of the for the approved record is greater than the effective_start date of the change
+							  request, then use that date as the effective_start date for the record being inserted.*/
+							case when cast(dateadd(minute, @number, r.created_date_utc) as date) > effective_start then cast(dateadd(minute, @number, r.created_date_utc) as date)
+								 else effective_start
+							end as effective_start,
+							l.change_request_id
+					 from sladb.dbo.tbl_change_request as l
+					 left join 
+						  sladb.dbo.tbl_change_request_status as r
+					 on l.change_request_id = r.change_request_id
+					 /*If the change request status (sla_change_status) is 2 = Approved and the effective_start_adj (adjusted effective_start_date)
+						 is equal to the current date minus one hour then insert the new record*/
+					 where l.sla_change_status = 2
 				/*Use an except operation to find only records that don't already exist in the tbl_unit_sla_season table. Except will only find the
 				  differences in rows between two results sets.*/
 				except
