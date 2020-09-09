2149c17154115c0e81876b8b575147b715c66463
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2020-09-09 13:43:14 -0400

    Added view for seeing change requests that are delayed. That is their effective_start_adj date is after the submitted and expected effective_start_adj date because the change requests were 1:Approved in a timely enough fashion to take effect on the desired date. Closes #163.

---
+++ vw_delayed_change_request.sql
@@ -0,0 +1,32 @@
+/***********************************************************************************************************************
+																													   	
+ Created By: Dan Gallagher, daniel.gallagher@parks.nyc.gov, Innovation & Performance Management         											   
+ Modified By: <Modifier Name>																						   			          
+ Created Date:  <MM/DD/YYYY>																							   
+ Modified Date: <MM/DD/YYYY>																							   
+											       																	   
+ Project: <Project Name>	
+ 																							   
+ Tables Used: <Database>.<Schema>.<Table Name1>																							   
+ 			  <Database>.<Schema>.<Table Name2>																								   
+ 			  <Database>.<Schema>.<Table Name3>				
+			  																				   
+ Description: <Lorem ipsum dolor sit amet, legimus molestiae philosophia ex cum, omnium voluptua evertitur nec ea.     
+	       Ut has tota ullamcorper, vis at aeque omnium. Est sint purto at, verear inimicus at has. Ad sed dicat       
+	       iudicabit. Has ut eros tation theophrastus, et eam natum vocent detracto, purto impedit appellantur te	   
+	       vis. His ad sonet probatus torquatos, ut vim tempor vidisse deleniti.>  									   
+																													   												
+***********************************************************************************************************************/
+use sladb
+go
+
+set ansi_nulls on;
+go
+
+set quoted_identifier on;
+go
+
+create or alter view dbo.vw_delayed_change_request as
+	select *
+	from sladb.dbo.tbl_change_request
+	where dbo.fn_getdate(effective_start, 1) != effective_start_adj;
