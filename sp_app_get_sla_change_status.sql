b8be6bc92e92bee443ff124c968b02e59ded0e63
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2020-12-23 16:30:07 -0500

    Added stored procedure to get the sla_change_status

---
+++ sp_app_get_sla_change_status.sql
@@ -0,0 +1,32 @@
+/***********************************************************************************************************************
+																													   	
+ Created By: Dan Gallagher, daniel.gallagher@parks.nyc.gov, Innovation & Performance Management         											   
+ Modified By: <Modifier Name>																						   			          
+ Created Date:  <MM/DD/YYYY>																							   
+ Modified Date: <MM/DD/YYYY>																							   
+											       																	   
+ Project: <Project Name>	
+ 																							   
+ Tables Used: <Database>.<Schema>.<Table Name1>																							   
+ 			  <Database>.<Schema>.<Table Name2>																								   
+ 			  <Database>.<Schema>.<Table Name3>				
+			  																				   
+ Description: <Lorem ipsum dolor sit amet, legimus molestiae philosophia ex cum, omnium voluptua evertitur nec ea.     
+	       Ut has tota ullamcorper, vis at aeque omnium. Est sint purto at, verear inimicus at has. Ad sed dicat       
+	       iudicabit. Has ut eros tation theophrastus, et eam natum vocent detracto, purto impedit appellantur te	   
+	       vis. His ad sonet probatus torquatos, ut vim tempor vidisse deleniti.>  									   
+																													   												
+***********************************************************************************************************************/
+use sladb
+go
+
+set ansi_nulls on;
+go
+
+set quoted_identifier on;
+go
+
+create or alter procedure dbo.sp_app_get_sla_change_status @app_perms_desc nvarchar(50) = null as
+	select *
+	from sladb.dbo.tbl_ref_sla_change_status
+	where sla_change_status != 1;
