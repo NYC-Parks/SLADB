2b396bfdda77c2507f99f994c3fc03cb282edcbd
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2020-12-23 15:51:21 -0500

    Added stored procedure to get the unit_id

---
+++ sp_app_get_unit_id.sql
@@ -0,0 +1,39 @@
+/***********************************************************************************************************************
+																													   	
+ Created By: Dan Gallagher, daniel.gallagher@parks.nyc.gov, Innovation & Performance Management         											   
+ Modified By: <Modifier Name>																						   			          
+ Created Date:  <MM/DD/YYYY>																							   
+ Modified Date: <MM/DD/YYYY>																							   
+											       																	   
+ Project: <Project Name>	
+ 																							   
+ Tables Used: <Database>.<Schema>.<Table Name1>																							   
+ 			  <Database>.<Schema>.<Table Name2>																								   
+ 			  <Database>.<Schema>.<Table Name3>				
+			  																				   
+ Description: <Lorem ipsum dolor sit amet, legimus molestiae philosophia ex cum, omnium voluptua evertitur nec ea.     
+	       Ut has tota ullamcorper, vis at aeque omnium. Est sint purto at, verear inimicus at has. Ad sed dicat       
+	       iudicabit. Has ut eros tation theophrastus, et eam natum vocent detracto, purto impedit appellantur te	   
+	       vis. His ad sonet probatus torquatos, ut vim tempor vidisse deleniti.>  									   
+																													   												
+***********************************************************************************************************************/
+use sladb
+go
+
+set ansi_nulls on;
+go
+
+set quoted_identifier on;
+go
+
+create or alter procedure dbo.sp_app_get_unit_id @sector nvarchar(50) as 
+	select r.unit_id,
+		   concat(r.unit_id, ' | ', r.unit_desc) as unit_desc
+	from (select *
+		  from [dataparks].dailytasks.dbo.ref_sector_districts
+		  where active = 1 and
+		        sector = @sector) as l
+	inner join
+		sladb.dbo.tbl_ref_unit as r
+	on l.district = r.unit_mrc
+	where lower(unit_status) != 'd';
